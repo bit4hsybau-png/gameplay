ae8f795d0f8e8d671d1e3997ad9812db9615ab31
bit4hsybau-png <bit4h.sybau@gmail.com>
2025-11-17 11:16:04 +0200

    Create icons.tsx

---
+++ components/icons.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+export const IconX = () => (
+  <svg className="w-full h-full" viewBox="0 0 100 100" stroke="currentColor" strokeWidth="12" fill="none" strokeLinecap="round">
+    <line x1="15" y1="15" x2="85" y2="85" />
+    <line x1="85" y1="15" x2="15" y2="85" />
+  </svg>
+);
+
+export const IconO = () => (
+  <svg className="w-full h-full" viewBox="0 0 100 100" stroke="currentColor" strokeWidth="12" fill="none">
+    <circle cx="50" cy="50" r="35" />
+  </svg>
+);
